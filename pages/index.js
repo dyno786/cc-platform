8e4d1a7c6465f93c5d93b38c29bcdad7e5b75bd8
dyno786 <mohammedadris108@gmail.com>
2026-04-01 23:38:35 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,9 @@
 import Head from 'next/head'
-import { useState } from 'react'
-import Layout from '../components/Layout'
-import Overview from '../components/tabs/Overview'
-import LocalSEO from '../components/tabs/LocalSEO'
-import Reviews from '../components/tabs/Reviews'
-import ComingSoon from '../components/tabs/ComingSoon'
+import dynamic from 'next/dynamic'
 
-export async function getServerSideProps() {
-  return { props: {} }
-}
+const App = dynamic(() => import('../components/App'), { ssr: false })
 
 export default function Home() {
-  const [activeTab, setActiveTab] = useState('overview')
-
-  function renderTab() {
-    switch (activeTab) {
-      case 'overview':    return <Overview onTabChange={setActiveTab} />
-      case 'local-seo':  return <LocalSEO />
-      case 'reviews':    return <Reviews />
-      default:           return <ComingSoon tab={activeTab} />
-    }
-  }
-
   return (
     <>
       <Head>
@@ -31,9 +13,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
       </Head>
-      <Layout activeTab={activeTab} onTabChange={setActiveTab}>
-        {renderTab()}
-      </Layout>
+      <App />
     </>
   )
 }
